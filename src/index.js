3dc1b698e66dae46b9de952ae578a1bed134b920
liujinmeng <1321839748@qq.com>
2017-12-03 16:41:56 +0800

    step 6

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 // 1. Initialize
 const app = dva();
 
-app.model(require("./models/users"));
+app.model(require('./models/users'));
 
 // 2. Plugins
 // app.use({});
--- src/routes/Users.js
+++ src/routes/Users.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import { connect } from 'dva';
 import styles from './Users.css';
+import UsersComponent from '../components/Users/Users';
 
 function Users() {
   return (
     <div className={styles.normal}>
-      Route Component: Users
+      <UsersComponent />
     </div>
   );
 }
 
-function mapStateToProps() {
-  return {};
-}
-
-export default connect(mapStateToProps)(Users);
+export default connect()(Users);
