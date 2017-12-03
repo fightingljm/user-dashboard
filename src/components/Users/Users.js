eeb4dc7b301f222e57464f189108cfd50dd7013b
liujinmeng <1321839748@qq.com>
2017-12-03 10:03:36 +0800

    step 5

---
+++ src/components/Users/Users.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Users.css';
+
+function Users() {
+  return (
+    <div className={styles.normal}>
+      Component: Users
+    </div>
+  );
+}
+
+export default Users;
